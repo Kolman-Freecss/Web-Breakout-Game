da512e8b17febf576ab510480ec05dff6a478c44
Sergio225 <s3rg10.martinez.roman@gmail.com>
2019-04-10 23:23:45 +0200

    Assets added, next I'll add the map design with the corresponsive dynamic and static body

--- js/configuration.js
+++ js/configuration.js
@@ -13,10 +13,15 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image("tile", "../assets/tile.png");
+    this.load.image("red_tile", "../assets/red_tile.png");
+    this.load.image("green_tile", "../assets/green_tile.png");
+    this.load.image("background", "../assets/background.jpg");
 }
 
 function create ()
 {
+    this.add.image(400, 300, "background");
 }
 
 function update ()
